c4dc0cea974a88fdb019c0e4752e9aa1d77bfe81
k-koech <kelvin.koech@student.moringaschool.com>
2021-06-19 10:35:31 +0300

    added mailchimp

--- static/js/script.js
+++ static/js/script.js
@@ -1,18 +1,20 @@
 $(document).ready(function(){
 $( ".service-img" ).click(function() 
 {
-    var closestClass = $(this).closest(".service-img");
-    closestClass.next(".service-content").toggle();
+    var closestImg = $(this).closest(".service-img");
+    closestImg.next(".service-content").show();
     // $( ".service-content" ).toggle();
-    closestClass.hide();
+    closestImg.hide();
    
 });
 
 $( ".service-content" ).click(function() 
 {
-    var closestClass = $(this).closest(".service-content");
-    $(".service-img").show();
-    closestClass.hide();
+    var closestContent = $(this).closest(".service-content");
+    // var closestIm = $(this).closest(".service-img");
+
+    closestIm.next(".service-img").show();
+    closestContent.next(".service-content").hide();
    
 });
 
@@ -26,10 +28,10 @@ $(document).ready(function(){
 $( ".portfolio-img" ).mouseenter(function() 
 {
     var closestCol = $(this).closest(".portfolio-img");
-    closestCol.next("h6").show();
+    closestCol.next(".caption").slideDown(100);
   })
   .mouseleave(function() {
-    $("h6" ).hide();
+    $(".caption" ).hide();
 });
 });
 
